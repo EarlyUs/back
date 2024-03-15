23b55bb2b90b204d44e2f7b208b1c052a4179a17
unknown <joayoung3@gmail.com>
2024-03-15 10:50:59 +0900

    set project

---
+++ src/test/java/com/earlyus/ewhanarae/EwhanaraeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.earlyus.ewhanarae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EwhanaraeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
