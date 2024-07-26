b1098c6faf85ce7ff6934498154c7004cb7e15fc
aqswa <joayoung3@gmail.com>
2024-07-26 12:46:17 +0900

    feat: cors config

---
+++ src/main/java/com/earlyus/ewhanarae/global/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.earlyus.ewhanarae.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter(){
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:3000/");
+        config.addAllowedOrigin("http://localhost:5173/");
+        config.addAllowedOrigin("https://earlyus-front.vercel.app/");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setMaxAge(3600L);
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
