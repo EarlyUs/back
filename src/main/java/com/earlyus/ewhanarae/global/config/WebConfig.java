9aeb0848c415c80c3e9b0839c31e5d5fd09347a1
johy <joayoung3@gmail.com>
2024-08-22 03:18:55 +0900

    fix: cors mappings

--- src/main/java/com/earlyus/ewhanarae/global/config/WebConfig.java
+++ src/main/java/com/earlyus/ewhanarae/global/config/WebConfig.java
@@ -1,18 +1,20 @@
 package com.earlyus.ewhanarae.global.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000/", "http://localhost:5173/", "https://earlyus-front.vercel.app/")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowCredentials(true)
-                .maxAge(3600);
+        registry
+                .addMapping("/**")
+                .allowedHeaders("*")
+                .allowedOrigins("http://localhost:3000", "http://localhost:5173", "https://earlyus-front.vercel.app")
+                .allowedMethods("*");
     }
 }
